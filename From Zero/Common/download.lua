6273f5a5ba754bac7e0ae49a83cffede1171c86e
GravityIO <harryioannoucy@gmail.com>
2022-08-01 21:06:24 +0300

    downlaod

--- From Zero/Common/download.lua
+++ From Zero/Common/download.lua
@@ -3,8 +3,6 @@ local args = {...};
 local repo = "https://raw.githubusercontent.com/GravityCY/cc-t/master/From%20Zero/"
 local path = args[1];
 
-local savePath = args[2]:lower() or args[1]:match("^.+/(.+)$"):lower();
-
+local savePath = args[2] or args[1]:match("^.+/(.+)$");
 if (fs.exists(savePath)) then fs.delete(savePath) end
-
 shell.run("wget", repo .. path, savePath);
