8c65f5625701d015bcef657aa0e4f6964debd8e6
GravityIO <harryioannoucy@gmail.com>
2022-08-01 22:21:55 +0300

    Mine better

--- From Zero/Turtle/Mine.lua
+++ From Zero/Turtle/Mine.lua
@@ -1,5 +1,6 @@
 local bt = require("bt");
 local tu = require("tu");
+local sides = bt.sides;
 
 local isForward = true;
 local mx, mz;
@@ -8,23 +9,21 @@ mx = tu.getInput(true, tu.types.number, "Enter Forward: ");
 mz = tu.getInput(true, tu.types.number, "Enter Right: ");
 
 local function right()
-  if (isForward) then turtle.turnRight();
-  else turtle.turnLeft(); end
+  if (isForward) then bt.turn(sides.right);
+  else bt.turn(sides.left); end
 end
 
 for z = 1, mz do
   for x = 1, mx do
-    turtle.dig();
-    turtle.forward();
-    turtle.digUp();
-    turtle.digDown();
+    bt.goDig(sides.forward);
+    bt.dig(sides.up);
+    bt.dig(sides.down);
   end
   if (z ~= mz) then
     right();
-    turtle.dig();
-    turtle.forward(); 
-    turtle.digUp();
-    turtle.digDown();
+    bt.goDig(sides.forward);
+    bt.dig(sides.up);
+    bt.dig(sides.down);
     right();
     isForward = not isForward;
   end
