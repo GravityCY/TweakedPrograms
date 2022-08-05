a0a55ccdd197744d42dfc8e13db31f1b71d337e8
GravityIO <harryioannoucy@gmail.com>
2022-08-05 06:28:15 +0300

    Download

--- From Zero/Common/download.lua
+++ From Zero/Common/download.lua
@@ -1,9 +1,8 @@
-local dapi = require("netutils");
+local netutils = require("netutils");
 local args = {...};
 
 local repo = "https://raw.githubusercontent.com/GravityCY/cc-t/master/From%20Zero/"
 local path = args[1];
 
 local savePath = args[2] or args[1]:match("^.+/(.+)$");
-if (fs.exists(savePath)) then fs.delete(savePath) end
-shell.run("wget", repo .. path, savePath);
+netutils.saveDownload(repo..path, savePath);
--- From Zero/Common/download1.lua
+++ From Zero/Common/download1.lua
@@ -1,10 +1,8 @@
+local netutils = require("netutils");
 local args = {...};
 
 local repo = "https://raw.githubusercontent.com/GravityCY/cc-t/master/Past/"
 local path = args[1];
 
 local savePath = args[2] or args[1]:match("^.+/(.+)$");
-local dfh = http.get(repo .. path);
-local data = dfh.readAll();
-local wfh = fs.open(savePath, "w");
-wfh.write(data);
+netutils.saveDownload(repo..path, savePath);
