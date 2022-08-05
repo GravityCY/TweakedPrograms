f7478621efb74434b30096d717bc6acca986ec2a
GravityIO <harryioannoucy@gmail.com>
2022-08-05 06:43:33 +0300

    net utils fix

--- From Zero/Common/lib/netutils.lua
+++ From Zero/Common/lib/netutils.lua
@@ -2,9 +2,11 @@ local t = {};
 
 function t.saveDownload(url, savePath)
   local resfile = http.get(url);
+  if (resfile == nil) then return end
   local data = resfile.readAll();
   local saveFile = fs.open(savePath, "w");
   saveFile.write(data);
+  saveFile.close();
 end
 
 return t;
