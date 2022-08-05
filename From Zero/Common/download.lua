f95b41ff465d3dc83b059583dda7c03327deacc4
GravityIO <harryioannoucy@gmail.com>
2022-08-05 06:53:31 +0300

    Fixes

--- From Zero/Common/download.lua
+++ From Zero/Common/download.lua
@@ -1,8 +1,11 @@
 local netutils = require("netutils");
+
 local args = {...};
 
 local repo = "https://raw.githubusercontent.com/GravityCY/cc-t/master/From%20Zero/"
 local path = args[1];
 
 local savePath = args[2] or args[1]:match("^.+/(.+)$");
-netutils.saveDownload(repo..path, savePath);
+if (netutils.saveDownload(repo..path, savePath)) then
+  print("Downloaded " .. savePath .. ".");
+else print("Couldn't Download."); end
--- From Zero/Common/lib/netutils.lua
+++ From Zero/Common/lib/netutils.lua
@@ -2,11 +2,12 @@ local t = {};
 
 function t.saveDownload(url, savePath)
   local resfile = http.get(url);
-  if (resfile == nil) then return end
+  if (resfile == nil) then return false end
   local data = resfile.readAll();
   local saveFile = fs.open(savePath, "w");
   saveFile.write(data);
   saveFile.close();
+  return true;
 end
 
 return t;
