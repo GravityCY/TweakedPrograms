5f1ca157f169e972f144c8b14bfcce41be172294
GravityIO <harryioannoucy@gmail.com>
2022-09-19 14:56:33 +0300

    Again

--- GravityIO/Common/Programs/SmartStorage/network/setup.lua
+++ GravityIO/Common/Programs/SmartStorage/network/setup.lua
@@ -16,5 +16,6 @@ NetUtils.saveDownload("https://raw.githubusercontent.com/GravityCY/cc-t/master/G
 NetUtils.saveDownload("https://raw.githubusercontent.com/GravityCY/cc-t/master/GravityIO/Common/Programs/SmartStorage/network/CraftingAPI.lua", "CraftingAPI.lua")
 NetUtils.saveDownload("https://raw.githubusercontent.com/GravityCY/cc-t/master/GravityIO/Common/lib/PerUtils.lua", "PerUtils.lua");
 NetUtils.saveDownload("https://raw.githubusercontent.com/GravityCY/cc-t/master/GravityIO/Common/lib/ItemUtils.lua", "ItemUtils.lua");
+NetUtils.saveDownload("https://raw.githubusercontent.com/GravityCY/cc-t/master/GravityIO/Common/lib/InvUtils.lua", "InvUtils.lua");
 NetUtils.saveDownload("https://raw.githubusercontent.com/GravityCY/cc-t/master/GravityIO/Common/lib/TableUtils.lua", "TableUtils.lua");
 NetUtils.saveDownload("https://raw.githubusercontent.com/GravityCY/cc-t/master/GravityIO/Common/lib/Localization.lua", "Localization.lua");
