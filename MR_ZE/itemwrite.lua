aaad319e54ba1a820eb3eafe66cabc8c141d48a7
GravityIO <harryioannoucy@gmail.com>
2022-09-20 15:43:21 +0300

    Removed Some Files

--- MR_ZE/itemwrite.lua
+++
@@ -1,9 +0,0 @@
-local barrel = peripheral.wrap("minecraft:barrel_2")
-local file = fs.open("item_data", "w")
- 
-for slot, item in pairs(barrel.list()) do
-    file.writeLine(slot)
-    file.writeLine(item.name)
-end
- 
-file.close()
